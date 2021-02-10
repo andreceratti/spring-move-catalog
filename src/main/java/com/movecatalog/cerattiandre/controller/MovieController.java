977e98b68749d085824aab8059b892597ab5e9f1
André Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2021-02-10 09:44:52 -0300

    Adicionado rota pra delete
    Adicionado nova rota para deletar filme, e corrigido nome do repositorio para o novo nome.

--- src/main/java/com/movecatalog/cerattiandre/controller/MovieController.java
+++ src/main/java/com/movecatalog/cerattiandre/controller/MovieController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,19 +13,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.movecatalog.cerattiandre.model.Movie;
-import com.movecatalog.cerattiandre.repository.MovieThumbRepository;
+import com.movecatalog.cerattiandre.repository.MovieRepository;
 
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
 	
 	@Autowired
-	private MovieThumbRepository movieThumbRepository;
+	private MovieRepository movieRepository;
 	
 	@GetMapping
 	public Optional<Movie> find(@RequestBody Movie request) {
 		String id = request.getId();
-		Optional<Movie> movieThumb = movieThumbRepository.findById(id);
+		Optional<Movie> movieThumb = movieRepository.findById(id);
 		return movieThumb;
 	}
 	
@@ -32,7 +33,14 @@ public class MovieController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Movie save(@RequestBody Movie request) {
 		System.out.println(request.getCast());
-		return movieThumbRepository.save(request);
+		return movieRepository.save(request);
+	}
+	
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public void delete(@RequestBody Movie request) {
+		String id = request.getId();
+		movieRepository.deleteById(id);
 	}
 
 }
