026cbe2bd1a7ba24bc2817249b68f065a295741e
André Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2021-02-10 15:28:56 -0300

    Pesquisar por nome
    Método para criado na interface para pesquisar gênero por nome

--- src/main/java/com/movecatalog/cerattiandre/repository/GenresRepository.java
+++ src/main/java/com/movecatalog/cerattiandre/repository/GenresRepository.java
@@ -1,9 +1,12 @@
 package com.movecatalog.cerattiandre.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.movecatalog.cerattiandre.model.Genre;
 
 public interface GenresRepository extends JpaRepository<Genre, String> {
-
+	
+	Genre findByName(String name);
 }
